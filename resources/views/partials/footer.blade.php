10f3ff558cd700fbb72f42ac649a95e66b68e9a2
Roman Sorin <rmaximsorin@gmail.com>
2019-08-11 21:47:00 -0400

    Removed "powered by" from footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,31 +4,24 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-sm-4">
-                @if($showSupport)
-                <p>
-                    {!! trans('cachet.powered_by') !!}
-                    @if($showTimezone)
-                    {{ trans('cachet.timezone', ['timezone' => $timezone]) }}
-                    @endif
-                </p>
-                @endif
-            </div>
-            <div class="col-sm-8">
+            <div class="col-sm-12">
                 <ul class="list-inline">
                     @if($currentUser || $dashboardLink)
                     <li>
-                        <a class="btn btn-link" href="{{ cachet_route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a>
+                        <a class="btn btn-link"
+                            href="{{ cachet_route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a>
                     </li>
                     @endif
                     @if($currentUser)
                     <li>
-                        <a class="btn btn-link" href="{{ cachet_route('auth.logout') }}">{{ trans('dashboard.logout') }}</a>
+                        <a class="btn btn-link"
+                            href="{{ cachet_route('auth.logout') }}">{{ trans('dashboard.logout') }}</a>
                     </li>
                     @endif
                     @if($enableSubscribers)
                     <li>
-                        <a class="btn btn-success btn-outline" href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
+                        <a class="btn btn-success btn-outline"
+                            href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
                     </li>
                     @endif
                 </ul>
